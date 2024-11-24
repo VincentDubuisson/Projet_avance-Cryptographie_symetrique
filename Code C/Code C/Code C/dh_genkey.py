0feae27efc33db31f944ca2ea218363a92ffacb3
igmoha <125393311+igmoha@users.noreply.github.com>
2024-11-24 15:13:26 +0100

    Update dh_genkey.py

--- Code C/Code C/Code C/dh_genkey.py
+++ Code C/Code C/Code C/dh_genkey.py
@@ -2,6 +2,7 @@ import sys
 import random
 from queue import Queue
 from threading import Thread
+from math import log
 
 # finction lecture parametre Diffie-hellman depuis un fichier 
 def lire_parametre(nomFichier):
@@ -24,21 +25,56 @@ def lire_parametre(nomFichier):
 def puissance_mod_n(a,e,n):
     return pow(a,e,n)
 
-def alice(queue,p,g,a):
+def alice(queue1,queue2,p,g,a):
     A=puissance_mod_n(g,a,p)
-    queue.put(A) #alice envoi A
+    queue1.put(A) #alice envoi A
     print("alice envoi A =", A)
-    B = queue.get() #alice recoit B
+    while (True):
+        B = list(queue2.queue) #alice recoit B 
+        if (B != []):
+            B=B[0]
+            print("alice a recu B :",B)
+            break       
     shared_key = puissance_mod_n (B,a,p)
     print("alice calcule la cle partager:", shared_key)
-
-def bob(queue,p,g,b):
-    A=queue.get()#bob recupere A
+    
+def bob(queue1,queue2,p,g,b):
+   
     B=puissance_mod_n(g,b,p)
-    queue.put(B)
+    queue2.put(B)
     print("bob envoi B =", B)
+    while (True):
+        A=list(queue1.queue)#bob recupere A
+        if (A !=[]):
+            A=A[0]
+            print ("bob a recu A:",A)
+            break
     shared_key = puissance_mod_n (A,b,p)
     print("bob calcule la cle partager:",shared_key)
+    
+
+def eve(queue1,queue2,p,g):
+    print(" eve connai les valeur p:",p," et g:",g)
+    print ("eve intersepte la comunication")
+    while (True):
+        A=list(queue1.queue)#eve essai d'intercepter A
+        if (A !=[]):
+            A=A[0]
+            print ("eve intersepte A=",A)
+            break
+    while (True):
+        B = list(queue2.queue) #eve essai d'intercepter B 
+        if (B!=[]):
+            B=B[0]
+            print("eve intersepte B=",B)
+            break 
+    print("S = a+b et d = a-b")  
+    S=log(A*B)/log(g)
+    d= log(A/B)/log(g)
+    print (S,d)
+    
+    print ("eve est incapable de retrouver la cle partager, a ni b")
+
 
 def main(param_fichier):
     p,g=lire_parametre(param_fichier)
@@ -50,14 +86,18 @@ def main(param_fichier):
     print("alice choisie a =", a ,"et bob b=",b)
 
 #creation file d'attente queue pour la comunication et thread pour l'execution de alice et bob en parallele
-    queue=Queue()
-    thread_alice=Thread(target=alice,args=(queue,p,g,a))
-    thread_bob=Thread(target=bob,args=(queue,p,g,b))
+    queue1=Queue()
+    queue2=Queue()
+    thread_alice=Thread(target=alice,args=(queue1,queue2,p,g,a))
+    thread_bob=Thread(target=bob,args=(queue1,queue2,p,g,b))
+    thread_eve=Thread(target =eve,args=(queue1,queue2,p,g))
 #demarage et attente fin execution des thread
     thread_alice.start()
     thread_bob.start()
-    thread_alice.join
+    thread_eve.start()
     thread_bob.join
+    thread_alice.join
+    thread_eve.join()
 
 if __name__ == "__main__":
     if len(sys.argv) <2:
